a8fb4b6f2901a468142681d4980603009e936fd9
corazon4815 <mango4815@gmail.com>
2021-09-08 11:07:55 +0900

    fix : 맞췄을때 카드 안사라지는 문제 수정

--- card.js
+++ card.js
@@ -51,7 +51,7 @@
         setTdTemplate : function(num) {
             let template = "";
             template += "<td class='cardNum'>"
-            template += "<img id='front"+num+"' class='card frontCard' src='image/"+num+".png'>"
+            template += "<img id='front"+num+"' class='card frontCard "+num+"' src='image/"+num+".png'>"
             template += "<img id='behind"+num+"' class='card behindCard' src='image/close.png' onclick='cardGame.event.openCard("+num+")'>"
             template += "</td>";
             return template;
@@ -62,39 +62,50 @@
 
         openedCard : 0,
 
-        fail: function() {
-            $('.frontCard').attr('style', "display:none;");
-            $('.behindCard').attr('style', "display:block;");
-        },
+        // fail: function() {
+        //     $('.frontCard').attr('style', "display:none;");
+        //     $('.behindCard').attr('style', "display:block;");
+        // },
 
-        success: function() {
-            $('.openedCard').attr('style', "display:none;")
-            $('#front'+num).attr('style', "display:none;");
-        },
+        // success: function(num) {
+        //     $('.openedCard').attr('style', "display:none;")
+        //     $('#front'+num).attr('style', "display:none;");
+        // },
 
-        setTime: function(callback) {
-            setTimeout(function() {
-                callback();
-            }, 800);
-        },
+        // setTime: function(callback) {
+        //     setTimeout(function() {
+        //         callback();
+        //     }, 800);
+        // },
 
         openCard: function(num) {
-            $('#front'+num).attr('style', "display:block;");
             $('#front'+num).addClass('openedCard');
+            $('#front'+num).attr('style', "display:block;");
             $('#behind'+num).attr('style', "display:none;");
-            
+            console.log(this.openedCard)
             if(this.openedCard == 0){
+                // $('.'+num).addClass('openedCard');
                 this.openedCard = num;
+                console.log(this.openedCard)    
+                
             }else {
                 if(num == this.openedCard + 40 || num == this.openedCard - 40){
-                    this.setTime(this.success);
+                    console.log(this.openedCard)
+                    setTimeout(function() {
+                        console.log(this.openedCard)
+                        $('.openedCard').attr('style', "display:none;")
+                        $('#front'+num).attr('style', "display:none;");
+                    }, 800);
                     this.openedCard = 0;
                 }else{
-                    this.setTime(this.fail);
-                    this.openedCard = 0;  
+                    console.log("틀림")
+                    setTimeout(function() {
+                        $('.frontCard').attr('style', "display:none;");
+                        $('.behindCard').attr('style', "display:block;");
+                    }, 800);
+                    this.openedCard = 0; 
                 }
             }
-            $('.frontCard').removeClass('openedCard');
         }
     };
 }(window, document));
